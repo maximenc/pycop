c754fb5cc65f7d531a8b8ae530857026802818cf
Maxime Nicolas <maxime.ncl+github@proton.me>
2022-11-20 19:21:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pycop',
-    version = '0.0.9',  
+    version = '0.0.10',  
     description = 'Copula for multivariate dependence modeling',
     long_description=open('README.md', 'r').read(),
     long_description_content_type="text/markdown",
