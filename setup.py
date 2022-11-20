d02d0169c2ee2f2a4b2e760417722a18bcee4916
Maxime Nicolas <maxime.ncl+github@proton.me>
2022-11-20 18:25:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version = '0.0.8',  
     description = 'Copula for multivariate dependence modeling',
     long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
     author = 'Maxime N',
     author_email = 'maxime.nlc@proton.me',
     url = 'https://github.com/maximenc/pycop/',
