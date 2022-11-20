3ccd136215f1c71177abcdcca5c7cd27056dfdd4
Maxime Nicolas <maxime.ncl+github@proton.me>
2022-11-20 18:11:16 +0100

    add setup.py & update pyproject.toml

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'pycop',
+    packages = ['pycop'],
+    version = '0.0.8',  
+    description = 'Copula for multivariate dependence modeling',
+    author = 'Maxime N',
+    author_email = 'maxime.nlc@proton.me',
+    url = 'https://github.com/maximenc/pycop/',
+    download_url = 'https://github.com/maximenc/pycop/',
+    classifiers = [],
+)
